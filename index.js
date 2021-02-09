9b193e13d4aa3bfcb29ede8199ead74654db966f
Fundnudel <git@fundnudel.net>
2021-02-10 00:02:15 +0100

    Implemented the Core module to the main file

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const client = new Discord.Client();
 require('dotenv').config();
 
 const messageHandler = require("./handler/MessageHandler.js");
+const Core = require("./core/bot.js")
 
 client.on('ready', () => {
+    Core.init();
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 //Message Event triggers the MessageHandler
 client.on('message', msg => {
-    let casino = new messageHandler(msg).init();
+    new messageHandler(msg).init();
 });
+
+module.exports = Core;
 
 client.login(process.env.DC_TOKEN);
